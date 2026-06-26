b8ebed33d31fa53fca9b7e320988c2cd2a272fa8
TatharNuar <TatharNuar@users.noreply.github.com>
2026-06-26 03:01:11 -0400

    Refactor Collatz module for improved syntax and logic
    * Remove unnecessary bitmask for evenness test
    * Improve optimization of line 9 like previous commit's optimization of line 7

--- samples/collatz.v
+++ samples/collatz.v
@@ -1,11 +1,11 @@
 
-module collatz(input clk, input start, input [15:0] data, output reg [15:0] val);
+module collatz (input clk, input start, input [15:0] data, output reg [15:0] val);
   always @(posedge clk) begin
     if(start) 
       val <= data;
-    else if(!(val & 1'b1))
-      val <= val >> 1;
+    else if(!val[0]) // even
+      val <= val >> 1; // x/2
     else
-      val <= val * 16'd3 + 16'd1;
+      val <= val + {val,1'b1}; // 3x+1
   end
 endmodule
